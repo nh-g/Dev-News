ac7c1bd5234335032a09fedf78163e47b01492f5
nh-g <giangngohong@gmail.com>
2021-04-12 14:45:32 +0200

    generated getter and setter for article's fields

--- src/main/java/se/sdaproject/Article.java
+++ src/main/java/se/sdaproject/Article.java
@@ -25,24 +25,36 @@ public class Article {
         this.authorName = authorName;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getBody() {
         return body;
     }
 
+    public void setBody(String body) {
+        this.body = body;
+    }
+
     public String getAuthorName() {
         return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
     }
 
 }
