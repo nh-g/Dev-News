c9b2c05be19d196177ec32f48acbdd8b8ac93803
nh-g <giangngohong@gmail.com>
2021-04-13 13:03:13 +0200

    created Topic Model

---
+++ src/main/java/se/sdaproject/Topic.java
@@ -0,0 +1,57 @@
+package se.sdaproject;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Topic {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JsonIdentityReference(alwaysAsId = true)
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    private List<Article> articles;
+
+    public Topic() {
+
+    }
+
+    public Topic(Long id, String name, List<Article> articles) {
+        this.id = id;
+        this.name = name;
+        this.articles = articles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+}
